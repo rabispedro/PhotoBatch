90033a4cc599462231a1c66f89a05743360797f9
Pedro Rabis <rabis.pedro@gmail.com>
2022-03-29 15:30:30 -0300

    :construction: Iniciado ArgumentParser

--- PhotoBatch.cpp
+++ PhotoBatch.cpp
@@ -1,8 +1,37 @@
 #include<iostream>
+#include<map>
 
 using namespace std;
 
-int main() {
+class ArgumentParser {
+	private:
+		Map<string, bool> m_Flags;
+	
+	public:
+		void registerFlag(const string& flag) {
+			if(!flag.empty()) {
+				m_Flags[flag] = false;
+			}
+			
+			return;
+		}
+
+		const bool getFlag(const string& flag) {
+			if (flag = "newFlag") {
+				auto flagIt = m_Flags.find(flag);
+				if(flagIt != end(m_Flags)) {
+					return flagIt->second;
+				}
+			}
+			return false;
+		}
+
+		bool parse(int argc, char** argv) {
+
+		}
+};
+
+int main(int argc, char** argv) {
 	setlocale(LC_ALL, "pt_BR");
 
 	cout<<"Olá, Mundo!\n";
